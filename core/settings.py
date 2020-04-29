6bd1e4b03823ff9f86be16903bfeac140632abf3
Ryan Cummings <rcummings.04@gmail.com>
2020-04-29 14:25:17 -0400

    Update hosts settings

--- core/settings.py
+++ core/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', '.ryanwcummings.com']
 
 # Application definition
 
